7e725d727b48735ee130cb25810167966b36be0e
Derek Wang <guanghuiw@vmware.com>
2023-09-01 16:03:12 +0800

    feat: add more custom operator

---
+++ src/app/shared/operators/index.ts
@@ -0,0 +1,5 @@
+export * from './api.operator';
+export * from './dg-state.operator';
+export * from './logger.operator';
+export * from './start-with-tap.operator';
+export * from './switchmap-with-loading.operator';
--- src/app/shared/operators/switchmap-with-loading.operator.ts
+++ src/app/shared/operators/switchmap-with-loading.operator.ts
@@ -2,6 +2,7 @@
  * https://dev.to/rensjaspers/beginners-guide-loading-data-based-on-changes-to-other-data-in-angular-106k
  */
 import { catchError, map, Observable, of, OperatorFunction, scan, startWith, switchMap } from 'rxjs';
+
 import { ApiQuery } from '../models/api-query';
 
 export function switchMapWithLoading<T, K>(observableFunction: (value: K) => Observable<T>): OperatorFunction<K, ApiQuery<T>> {
